5a10d06e8e666df68e718df5ec8861d1c20a76fa
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-06 15:58:39 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 from config import get_config
 
+# Initialize extensions outside of the create_app function
 db = SQLAlchemy()
 jwt = JWTManager()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(get_config())
 
+    # Initialize extensions with the app inside the create_app function
     db.init_app(app)
     jwt.init_app(app)
+    migrate.init_app(app, db)  # Initialize Flask-Migrate after the db
 
     from . import models, routes, services
+    routes.init_app(app)
     return app
+
